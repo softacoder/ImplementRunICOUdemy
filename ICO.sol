502bc08a9c3139e5674de637aa9ab1ef7193f979
softacoder <113185323+softacoder@users.noreply.github.com>
2023-09-18 20:54:46 +0100

    commit 1/2 way through the code

--- ICO.sol
+++ ICO.sol
@@ -1,15 +1,15 @@
-// SPDX-License_Identifier: GPL-3.0
+// "SPDX-License_Identifier: GPL-3.0" "SPDX-License-Identifier: UNLICENSED"
 
 pragma solidity >=0.5.0 <0.9.0;
 // ----------------------------------
 // EIP-20: ERC-20 Token Standard
 // https://eips.ethereum.org/EIPS/eip-20
 // ----------------------------------
-
 interface ERC20Interface {
     function totalSupply() external view returns(uint);
     function balanceOf(address tokenOwner) external view returns(uint balance);
     function transfer(address to, uint tokens) external returns(bool success);
+
 
     function allowance(address tokenOwner, address spender) external view returns (uint remaining);
     function approve(address spender, uint tokens) external returns (bool success);
@@ -18,6 +18,71 @@ interface ERC20Interface {
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
+
+contract Cryptos is ERC20Interface{
+    string public name = "Cryptos";
+    string public symbol = "CRPT";
+    uint public decimals = 0; //18
+    uint public override totalSupply;
+
+    address public founder;
+    mapping (address => uint) public balances;
+
+    mapping (address => mapping(address => uint)) allowed;
+
+    constructor() {
+    totalSupply = 1000000;
+    founder = msg.sender;
+    balances[founder] = totalSupply;
+}
+
+
+    function balanceOf(address tokenOwner) public view override returns (uint balance){
+        return balances [tokenOwner];
+    }
+
+
+    function transfer(address to, uint tokens) public override returns (bool success){
+        require(balances[msg.sender] >= tokens);
+
+    balances[to] += tokens;
+    balances[msg.sender] -= tokens;
+    emit Transfer(msg.sender, to, tokens);
+
+    return true;
+
+    }
+
+
+    function allowance(address tokenOwner, address spender) view public override returns(uint){
+        return allowed[tokenOwner][spender];
+    }
+
+
+    function approve(address spender, uint tokens) public override returns(bool success){
+        require(balances[msg.sender] >= tokens);
+        require (tokens > 0);
+
+        allowed[msg.sender][spender] = tokens;
+
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+
+    }
+
+    function transferFrom(address from, address to, uint tokens) public override returns (bool success){
+        require (allowed[from][to] >= tokens);
+        require (balances[from] >= tokens);
+
+    balances[from] -= tokens;
+    balances[to] += tokens;
+    allowed[from][to] -= tokens;
+
+    return true;
+
+    }
+}
+
 
 contract CryptosICO is Cryptos{
     address public admin;
@@ -35,23 +100,28 @@ contract CryptosICO is Cryptos{
     State public icoState;
 
     constructor (address payable _deposit){
-        deposit = deposit;
+        deposit = _deposit;
         admin = msg.sender;
         icoState = State.beforeStart;
     } 
+
     modifier onlyAdmin(){
         require(msg.sender == admin);
         _;
     }
+
     function halt() public onlyAdmin{
-        icoState = state.halted;
+        icoState = State.halted;
     }
+
     function resume() public onlyAdmin{
-        icoState = state.running;
+        icoState = State.running;
     }
+
     function changeDepositAddress(address payable newDeposit) public onlyAdmin{
         deposit = newDeposit;
     } 
+
     function getCurrentState() public view returns(State){
         if(icoState == State.halted){
             return State.halted;
@@ -64,14 +134,17 @@ contract CryptosICO is Cryptos{
 }
 }
 
+event Invest(address investor, uint value, uint tokens);
+
 function invest() payable public returns(bool){
     icoState = getCurrentState();
     require(icoState == State.running);
 
     require(msg.value >= minInvestment && msg.value <= maxInvestment);
+    raisedAmount += msg.value;
     require(raisedAmount <= hardCap);
 
-    uint tokens = msg.sender / tokenPrice;
+    uint tokens = msg.value / tokenprice;
 
     balances[msg.sender] += tokens;
     balances[founder] -= tokens;
@@ -80,6 +153,7 @@ function invest() payable public returns(bool){
 
     return true;
 }
+
 receive() payable external{
     invest();
 }
